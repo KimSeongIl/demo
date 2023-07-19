5969f8d4d1859d682f7393313494dbb91648b816
alan.s <alan.s@kakaoenterprise.com>
2023-07-19 10:30:03 +0900

    add resources

--- src/main/kotlin/com/demo/search/middle/demo/DemoConfiguration.kt
+++ src/main/kotlin/com/demo/search/middle/demo/DemoConfiguration.kt
@@ -1,9 +1,12 @@
 package com.demo.search.middle.demo
 
+import jakarta.validation.constraints.Pattern
+import org.springframework.aot.hint.annotation.RegisterReflectionForBinding
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration
 
 @EnableConfigurationProperties(DemoContext::class)
+@RegisterReflectionForBinding(Pattern::class, Pattern.Flag::class)
 @Configuration
 class DemoConfiguration(private val demoContext: DemoContext) {
 
---
+++ src/main/kotlin/com/demo/search/middle/demo/Test.java
@@ -0,0 +1,9 @@
+package com.demo.search.middle.demo;
+
+public class Test {
+    private DemoContext test;
+
+    public void setTest(String test) {
+        this.test = new DemoContext();
+    }
+}
